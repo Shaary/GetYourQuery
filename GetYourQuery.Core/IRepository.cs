fe6553b3d8b197c0cf5969dd0d8c887b5f4017d6
Shaary <kallisto3261@outlook.com>
2020-05-09 20:58:42 -0600

    Add project files.

---
+++ GetYourQuery.Core/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace GetYourQuery.Core
+{
+    public interface IRepository
+    {
+        DataTable ParametersTableGet();
+        string ParametersDataGet(Dictionary<string, ColumnTablePair> paramColumnTable);
+    }
+}
---
+++ GetYourQuery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetYourQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
